06e5a141e68b783a1913305ee42c62d5aa551c32
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2026-01-08 14:40:26 -0800

    set up basic form

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Defrost</h1>
+      <h4>Discard the Frost.</h4>
+      <div class="register">
+        <h2>Sign up Today!</h2>
+        <label for="phone-number">
+          Your Phone Number:
+          <input type="number" name="phone-number"/>
+        </label>
+        <button class="btn">Sign up</button>
+      </div>
+    </>
+  )
+}
+
+export default App
